56b8c98aa67c4efc4cabd4d65809f4ac28b027fe
kjgorman <kieran@kjgorman.com>
2013-05-04 17:17:56 +1200

    clean up that todo by actually using instanceof

--- Melchior/JS/Selectors.js
+++ Melchior/JS/Selectors.js
@@ -93,9 +93,7 @@ var Selectors = function () {
 
 
     Selector.prototype.toDocument = function(elem) {
-        //the assumption here being that only the document has
-        //Selector.prototype defined... #TODO - probably an actual value to check
-        return elem && elem.doctype !== undefined ? elem : null
+        return elem ? elem instanceof HTMLDocument ? elem : null : null
     }
     
     return new Selector()
