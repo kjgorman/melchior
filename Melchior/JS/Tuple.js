d8ddf46cb6fd8180986bf39727015fe7cc37f713
kieran gorman <kieran.j.gorman@gmail.com>
2013-05-09 00:09:35 +1200

    Update Tuple.js

--- Melchior/JS/Tuple.js
+++ Melchior/JS/Tuple.js
@@ -3,6 +3,7 @@ var Tuples = function () {
 
     var Tuple = function () { }
 
+    //TODO --- implement ampersand properly
     Tuple.prototype.pair = function (tpl) {
         console.log("tuple: ", tpl)
         return tpl
