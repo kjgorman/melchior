f6342d72241976246fbd2e4487f5cd542f0ffc37
kjgorman <kieran@kjgorman.com>
2013-05-27 02:19:47 +1200

    clean up control

--- Melchior/Control.hs
+++ Melchior/Control.hs
@@ -13,11 +13,6 @@ module Melchior.Control
   , (<<<)
   , (&&&)
   , pipe
-  , source
-  , passThrough
-  , signalIO
-  , terminal
-  , liftSignal
   , ensureApplicable
   ) where
 
@@ -56,28 +51,13 @@ s &&& t = \x -> primAmpersands s t x
 foreign import js "Tuples.pair(%1, %2, %3)"
   primAmpersands :: SF a b -> SF a c -> Signal a -> Signal (b, c)
 
-foreign import js "Tuples.pair(%1)"
-  pair :: JSPtr a -> a
-
-foreign import js "Signals.source(%1)"
-  source :: Signal a -> Element
-
-foreign import js "Signals.signalIO(%1)"
-  signalIO :: IO a -> IO a
-
 foreign import js "Signals.applicable(%1)"
   applicable :: a -> a
 
 -- * Routing
 
-terminal :: SF a a 
-terminal s = (\x -> ensureApplicable $ pass (stringToJSString "terminal") $! x) <$> s
-
 foreign import js "Signals.ensureApplicable(%1)"
   ensureApplicable :: a -> a
-
-passThrough :: a -> b -> a
-passThrough x y =  y `seq` (applicable x)
 
 pipe :: (a -> b) -> Signal a -> Signal b
 pipe f s = primPipeSignal s f
@@ -92,9 +72,6 @@ foreign import js "%1.pipe(%2)"
   primPipeSignalWithEvent :: Signal a -> (a -> Event c -> b) -> Signal b
 
 -- * Signal creation
-
-liftSignal :: Signal a -> Signal (Dom a)
-liftSignal s = (\x -> pass (stringToJSString "lifted") $ return $! x) <$> s
 
 foldP :: (a -> b -> b) -> b -> Signal a -> Signal b
 foldP fn start signal = createPastDependentSignal fn start signal
