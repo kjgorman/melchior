58be7f92dd10f22d335f5687760e09973c45f501
kjgorman <kieran@kjgorman.com>
2013-06-18 23:11:13 +1200

    work on modifying the sorting thing to something cooler -- and this will also give me a chance to look at the parsing stuff in more granular detail

--- example/assets/hs/sorting.hs
+++ example/assets/hs/sorting.hs
@@ -5,11 +5,13 @@ import Control.Applicative
 import Prelude hiding ((.), id, head, map)
 
 import Melchior.Control
+import Melchior.Data.String
 import Melchior.Dom
 import Melchior.Dom.Events
 import Melchior.Dom.Html
 import Melchior.Dom.Selectors
 import Melchior.EventSources.Mouse
+import Melchior.Remote.Internal.ParserUtils
 import Melchior.Time
 
 main :: IO Element
@@ -17,9 +19,17 @@ main = runDom setupSorting
 
 setupSorting :: [Element] -> Dom Element
 setupSorting html = do
+  inp <- Dom $ select (byId "inp" . from) html >>= \m -> return $ ensures m
   ordering <- Dom $ select (byId "numbers" . from) html >>= \m -> return $ ensures m
-  put ordering ((\_ -> sample $ constant (qsort [3,4,7,2,4,5,7,43,2,5,7,9,5,3,2,5,7,9,4,2,4,6,7])) <$> every second)
+  input <- return $ createEventedSignal (Of "string") inp (ElementEvt InputEvt)
+  put ordering ((\_ -> ensureApplicable $ qsort $ parse $ value $ toInput inp) <$> input)
   return $ UHC.Base.head html
+
+stringListToNumbers :: String -> [Int]
+stringListToNumbers s = [2,3,1]
+
+parse :: JSString -> [Int]
+parse s = stringListToNumbers $ jsStringToString s
 
 qsort :: Ord a => [a] -> [a]
 qsort [] = []
@@ -28,5 +38,5 @@ qsort (pivot:rest) = (qsort lesser) ++ [pivot] ++ (qsort greater)
     lesser = filter (< pivot) rest
     greater = filter (>= pivot) rest
 
-put :: (Renderable a) => Element -> Signal a -> Dom ()
+put :: (Renderable a) => Element -> Signal (a) -> Dom ()
 put el s = terminate s (\x -> return $ set el "innerHTML" $ render x)
