909cd81c908944431dc43f4f846d276d6c0820fc
kjgorman <kieran@kjgorman.com>
2013-06-25 14:26:05 +1200

    Rename to an individual example name

---
+++ comparison/fauxter/resources/flight/app/app.js
@@ -0,0 +1,15 @@
+
+define (
+    [
+        "./ui/switcher",
+        "./ui/tweeter"
+    ],
+    function (Switcher, Tweeter) {
+        
+        var initialize = function () {
+            Switcher.attachTo(".link");
+            Tweeter.attachTo(document);
+        };
+        return { initialize: initialize }
+    }
+);
---
+++ comparison/fauxter/server.js
@@ -0,0 +1,37 @@
+var express = require('express')
+  , faker = require('./Faker')
+  , app = express();
+
+var count = 0;
+
+app.use("/resources", express.static(__dirname+"/resources"));
+
+app.get("/homes", function(req, res) {
+    res.send(200, {homes: [{
+      id: ++count,
+      author: faker.Name.findName(),
+      body: faker.Lorem.words(10)
+    }]});
+});
+
+app.get("/", function(req, res) {
+    res.sendfile("index.html");
+});
+
+app.get("/ember", function(req, res) {
+    res.sendfile("resources/ember/index.html");
+});
+
+app.get("/jquery", function(req, res) {
+    res.sendfile("resources/jquery/index.html");
+});
+
+app.get("/flight", function(req, res) {
+    res.sendfile("resources/flight/index.html");
+});
+
+app.get("/knockout", function(req, res) {
+    res.sendfile("resources/knockout/index.html");
+});
+
+app.listen(3002);
