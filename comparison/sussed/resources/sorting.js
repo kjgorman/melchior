71dc776ce1a9ae4e2f65dabe99fd55954eb7f588
kjgorman <kieran@kjgorman.com>
2013-06-26 15:48:13 +1200

    javascript merge sort

--- comparison/sussed/resources/sorting.js
+++ comparison/sussed/resources/sorting.js
@@ -2,8 +2,10 @@
     var inp = document.getElementById("inp")
 
     inp.addEventListener("input", function(e) {
-        var out = document.getElementById("js-quick")
-        out.innerHTML = quicksort(parse(inp.value))
+        var qout = document.getElementById("js-quick")
+        qout.innerHTML = quicksort(parse(inp.value))
+        var mout = document.getElementById("js-merge")
+        mout.innerHTML = mergesort(parse(inp.value))
     })
 
     function quicksort(values) {
@@ -14,6 +16,37 @@
             else greater.push(v)
         })
         return quicksort(lesser).concat([pivot]).concat(quicksort(greater))
+    }
+
+    function merge(a, b) { //mmm... imperative
+        var results = []
+        while(a.length > 0 || b.length > 0) {
+            if(a.length > 0 && b.length > 0) {
+                if(a[0] < b[0]) {
+                    results.push(a[0])
+                    a.shift()
+                } else {
+                    results.push(b[0])
+                    b.shift()
+                }
+            } else if (a.length > 0) {
+                results.push(a[0])
+                a.shift()
+            } else {
+                results.push(b[0])
+                b.shift()
+            }
+        }
+        return results
+    }
+
+    function mergesort(values) {
+        if(values.length <= 1) return values
+        var left = values.slice(0, values.length/2)
+        var right = values.slice(values.length/2)
+        left = mergesort(left)
+        right = mergesort(right)
+        return merge(left, right)
     }
 
     function parse(value) {
