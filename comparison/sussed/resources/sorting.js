8bfdd7f3d2ac1387e0e1b641e4bd01e03440731e
kjgorman <kieran@kjgorman.com>
2013-06-26 16:53:35 +1200

    js heap sort

--- comparison/sussed/resources/sorting.js
+++ comparison/sussed/resources/sorting.js
@@ -6,6 +6,8 @@
         qout.innerHTML = quicksort(parse(inp.value))
         var mout = document.getElementById("js-merge")
         mout.innerHTML = mergesort(parse(inp.value))
+        var hout = document.getElementById("js-heap")
+        hout.innerHTML = heapsort(parse(inp.value))
     })
 
     function quicksort(values) {
@@ -47,6 +49,44 @@
         left = mergesort(left)
         right = mergesort(right)
         return merge(left, right)
+    }
+
+    function heapsort(values) {
+        heapify(values)
+        var end = values.length-1
+        while(end > 0) {
+            swap(values, end, 0)
+            end -= 1
+            siftDown(values, 0, end)
+        }
+        return values
+    }
+
+    function heapify(values) {
+        var start = Math.floor((values.length-1)/2)
+        while(start >= 0) {
+            siftDown(values, start, values.length-1)
+            start -= 1
+        }
+    }
+    function siftDown(values, start, end) {
+        var root = start
+        while(root*2+1 <= end) {
+            var child = root * 2 + 1
+              , i = root
+            if(values[i] < values[child]) i = child
+            if(child+1 <= end && values[i] < values[child+1]) i = child+1
+            if(i != root) {
+                swap(values, root, i)
+                root = i
+            } else return
+        }
+    }
+
+    function swap(values, i, j) {
+        var t = values[i]
+        values[i] = values[j]
+        values[j] = t
     }
 
     function parse(value) {
