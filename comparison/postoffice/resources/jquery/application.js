06954a1503cc7091c9edd135911327454e896073
kjgorman <kieran@kjgorman.com>
2013-06-28 17:15:14 +1200

    postoffice jquery inbox

--- comparison/postoffice/resources/jquery/application.js
+++ comparison/postoffice/resources/jquery/application.js
@@ -1,17 +1,29 @@
 $(document).ready(function () {
 
+    var socket = io.connect("/")
+    sendComposedMail(socket)
+    receiveMail(socket)
+
+});
+
+function sendComposedMail(socket) {
     $("#submit").on("click", function () {
         if($("#writer").val() == "") return
         $.ajax({
             method:"POST",
             type:"JSON",
             url:"/send",
-            data:{data:$("#writer").val()},
+            data:{data:$("#writer").val(), socket:socket.socket.sessionid},
             success: function(r) {
                 if(r.status == "ok")
                     $("#outbox").prepend($("<li>"+$("#writer").val()+"</li>"))
             }
         })
     })
+}
 
-});
+function receiveMail(socket) {
+    socket.on("/receive", function(d) {
+        $("#inbox").prepend($("<li>"+d.message+"</li>"))
+    })
+}
