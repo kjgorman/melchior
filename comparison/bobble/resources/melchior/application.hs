5a2a4d50910196af553605f3629bfbd3993cc396
kjgorman <kieran@kjgorman.com>
2013-07-04 17:01:33 +1200

    sensible movement

--- comparison/bobble/resources/melchior/application.hs
+++ comparison/bobble/resources/melchior/application.hs
@@ -16,13 +16,13 @@ main = runDom setupBobble
 frame = 17 -- ~16.66ms / 60fps
 
 data Game   = Game Player Player Ball
-data Player = Player Int Int Int
+data Player = Player { x :: Int, y :: Int, vx :: Float, vy :: Float, score :: Int }
 data Ball   = Ball Int Int Float Float
 
 setupBobble html = do
   canvas <- Dom $ assuredly $ select (canvases . byId "canvas" . from) html
   context <- return $ contextOf canvas
-  game <- return $ Game (Player 50 350 0) (Player 650 350 0) (Ball 350 50 1 1)
+  game <- return $ Game (Player 50 350 0 0 0) (Player 650 350 0 0 0) (Ball 350 50 1 1)
   keys <- return $ keyCode $ keyDownSignal (toElement document)
   play game context keys
   return $ toElement canvas
@@ -31,7 +31,7 @@ play :: Game -> Context -> Signal Int -> Dom ()
 play g context keys = terminate (after frame)
                       (\_ -> do
                           drawBackground context
-                          h <- return $ collide (move g $ takes keys)
+                          h <- return $ collide $ move g (takes keys)
                           drawGameElements h context
                           let Dom io = play h context keys in io)
 
@@ -48,7 +48,7 @@ drawGameElements (Game p1 p2 b) c = do
   drawBall b (fillStyle c green)
 
 drawPlayer :: Player -> Context -> IO ()
-drawPlayer (Player x y _) c = let Dom io = circle x y 25 c in io
+drawPlayer p c = let Dom io = circle (x p) (y p) 25 c in io
 
 drawBall :: Ball -> Context -> IO ()
 drawBall (Ball x y _ _) c = let Dom io = circle x y 10 c in io
@@ -57,27 +57,31 @@ collide :: Game -> Game
 collide (Game p1 p2 b) = Game (collideP p1) (collideP p2) (collideB p1 p2 b)
 
 collideP :: Player -> Player
-collideP (Player x y s) = case (abs $ x - 375)  < 25 of
-  True -> if x > 375 then Player (x + 10) y s else Player (x - 10) y s
-  False -> Player x y s
+collideP p = if ((abs $ (x p) - 375) < 25) || ((x p) < 25) || ((x p) > 700)
+             then Player (x p) (y p) ((vx p)* (-1)) (vy p) (score p)
+             else p
 
 collideB :: Player -> Player -> Ball -> Ball
 collideB _ _ b = b
 
 move :: Game -> Int -> Game
-move (Game p1 p2 b) i = case i > 39 of
-  True -> Game (moveP p1 i) p2 $ moveB b
-  False -> Game p1 (moveP p2 i) $ moveB b
-
+move (Game p1 p2 b) i = if i > 40
+                        then Game (gravitate $ moveP p1 i) (gravitate $ moveP p2 (-1)) (moveB b)
+                        else Game (gravitate $ moveP p1 (-1)) (gravitate $ moveP p2 i) (moveB b)
+dv = 5.0
 moveP :: Player -> Int -> Player
-moveP (Player x y s) i = case i of
-  37 -> (Player (x - 5) y s)
-  65 -> (Player (x - 5) y s)
-  38 -> (Player x (y - 5) s)
-  87 -> (Player x (y - 5) s)
-  39 -> (Player (x + 5) y s)
-  68 -> (Player (x + 5) y s)
-  _  -> (Player x y s)
+moveP p i = if (i == 37 || i == 65)
+            then Player ((x p) + (round (vx p))) (y p) ((vx p) - dv) (vy p) (score p)
+            else if (i == 38 || i == 87)
+                 then (if (y p) == 350
+                       then Player (x p) ((y p) + (round (vy p))) (vx p) ((vy p) - 5*dv) (score p)
+                       else Player ((x p) + (round (vx p))) ((y p) + (round (vy p))) ((vx p)*0.9) ((vy p)*0.9) (score p))
+                 else
+                   if (i == 39 || i == 68)
+                   then Player ((x p) + (round (vx p))) (y p) ((vx p) + dv) (vy p) (score p)
+                   else Player ((x p) + (round (vx p))) ((y p) + (round (vy p))) ((vx p)*0.9) ((vy p)*0.9) (score p)
+
+gravitate (Player x y vx vy s) = Player x (if y > 350 then 350 else y) vx (vy+0.5) s
 
 moveB :: Ball -> Ball
-moveB (Ball x y vx vy) = Ball (x+(floor vx)) (y+(floor vy)) (vx) (vy)
+moveB (Ball x y vx vy) = Ball (x+(round vx)) (y+(round vy)) (vx) (vy)
