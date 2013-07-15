1020b867ca1425e355b1ba9e7e19ecfcd76670d1
kjgorman <kieran@kjgorman.com>
2013-07-15 15:31:27 +1200

    The rest of the concatenations in Haskell

--- comparison/typesafe/concatenation/concatenationhs.hs
+++ comparison/typesafe/concatenation/concatenationhs.hs
@@ -14,7 +14,14 @@ main = runDom setupComparisons
 setupComparisons :: [Element] -> Dom Element
 setupComparisons html = do
   output <- Dom $ select (byId "hs-output" . from) html
-  appendConcatenation output "String to String" (stringToString "hello " "world")
+  appendConcatenation output "String to String" $ stringToString "hello " "world"
+  appendConcatenation output "Integer to String" $ intToString 5 " + 5"
+  appendConcatenation output "ADT to String" $ objectToString (Datum "hello") " world"
+  appendConcatenation output "List to ADT" $ listToObject ["hello", "world"] (Datum "how are you")
+  appendConcatenation output "Int to ADT" $ intToObject 42 (Datum " the answer, etc")
+  appendConcatenation output "Int to List" $ intToList 42 [43, 44]
+  appendConcatenation output "ADT to List" $ objectToList (Datum "foo") ["bar", "quux"]
+  appendConcatenation output "List to string" $ listToString ["e.g.", "i.e"] "viz."
   return $ UHC.Base.head html
 
 appendConcatenation :: Maybe Element -> String -> String -> Dom ()
@@ -26,6 +33,30 @@ container s t = JDiv [label, result]
                 where label = addClassTo "label" $ JDiv [Text $ stringToJSString s]
                       result = addClassTo "concat" $ JDiv [Text $ stringToJSString t]
 
-
 stringToString :: String -> String -> String
 stringToString s t = s ++ t
+
+intToString :: Int -> String -> String
+intToString i s = (show i) ++ s
+
+data Datum = Datum String
+instance Show Datum where
+  show (Datum s) = "[Datum: "++s++"]"
+
+objectToString :: Datum -> String -> String
+objectToString d s = (show d) ++ s
+
+listToObject :: Show a => [a] -> Datum -> String
+listToObject xs d = (show xs) ++ (show d)
+
+intToObject :: Int -> Datum -> String
+intToObject i d = (show i) ++ (show d)
+
+intToList :: Show a => Int -> [a] -> String
+intToList i xs = (show i) ++ (show xs)
+
+objectToList :: Show a => Datum -> [a] -> String
+objectToList d xs = (show d) ++ (show xs)
+
+listToString :: Show a => [a] -> String -> String
+listToString xs s = (show xs) ++ s
