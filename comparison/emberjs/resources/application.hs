8623439c710718d7a20361a68add680c99973965
kjgorman <kieran@kjgorman.com>
2013-09-01 21:37:09 +1200

    gravatar impl.

--- comparison/emberjs/resources/application.hs
+++ comparison/emberjs/resources/application.hs
@@ -5,13 +5,21 @@ import Prelude hiding ((.), id)
 import Control.Applicative
 
 import Melchior.Control
+import Melchior.Crypto
+import Melchior.Data.String
 import Melchior.Dom
 import Melchior.Dom.Selectors
 import Melchior.EventSources.Elements
 import Melchior.Sink
+import Melchior.Time
 
 main :: IO ()
-main = runDom echoes
+main = runDom ember
+
+ember :: [Element] -> Dom ()
+ember html = do
+  gravatar html
+  echoes html
 
 echoes :: [Element] -> Dom ()
 echoes html = do
@@ -24,3 +32,21 @@ echo i o = case echoed of
   Just x -> x
   Nothing -> return ()
   where echoed = o >>= \x -> i >>= \y -> Just $ put x $ inputValue y
+
+gravatar :: [Element] -> Dom ()
+gravatar html = do
+  gravInput <- Dom $ select (inputs . byId "gravatar-id" . from) html
+  gravImg <- Dom $ select (images . byId "gravatar-img" . from) html
+  retrieve gravInput gravImg
+
+
+retrieve :: Maybe Input -> Maybe Image -> Dom ()
+retrieve i o = case values of
+  Nothing -> return ()
+  Just x -> x
+  where values = i >>= \x -> o >>= \y -> Just $ terminate (throttle second $ inputValue x) $ setUrl y
+        setUrl el = (\s -> return $ set (toElement el) "src" $ path s)
+        path s = stringToJSString $ pre ++ (hash s) ++ post
+        hash s = md5 $ jsStringToString s
+        pre = "http://www.gravatar.com/avatar/"
+        post = "?s=200"
